3e91d28de6c8dc75c8f79ffab9a447ab67e5a984
jiyoung <jjiyoung0421@gmail.com>
2024-08-18 01:09:49 +0900

    박스 선택 색깔 적용, 버스 이동

--- static/js/script.js
+++ static/js/script.js
@@ -1,45 +1,33 @@
+const selectedValues = {}; // 전역 변수로 선언
+
 document.addEventListener("DOMContentLoaded", () => {
-  const selectedValues = {};
-  // Object to store the selected values, keyed by dropdown data-key
+    const questionBoxes = document.querySelectorAll(".question-box");
 
-  // Find all dropdown items
-  const questionBoxes = document.querySelectorAll(".question-box");
-
-  // Add click event listeners to each dropdown item
-  questionBoxes.forEach((item) => {
-    item.addEventListener("click", function (event) {
-      event.preventDefault(); // Prevent default link behavior
-
-      // Retrieve the text content of the clicked item and the dropdown data-key
-
-      const questionArea = this.closest(".question-area");
-      const questionKey = questionArea.getAttribute("data-question");
-
-      // Update the dropdown button text to show the selected item
-      // dropdownButton.textContent = value;
-
-      // Update the selected value for this dropdown in selectedValues object
-
-      const value = this.getAttribute("data-value");
-      selectedValues[questionKey] = value;
-
-      // Remove 'active' class from all items in the current dropdown
-      // const dropdownMenu = this.closest('.dropdown-menu');
-      // dropdownMenu.querySelectorAll('.dropdown-item').forEach(item => {
-      //     item.classList.remove('active');
-      // });
-
-      questionArea.querySelectorAll(".question-box").forEach((box) => {
-        box.classList.remove("active");
+    questionBoxes.forEach((item) => {
+      item.addEventListener("click", function (event) {
+        event.preventDefault();
+  
+        const questionArea = this.closest(".question-area");
+        const questionKey = questionArea.getAttribute("data-question");
+        const value = this.getAttribute("data-value");
+        selectedValues[questionKey] = value;
+  
+        questionArea.querySelectorAll(".question-box").forEach((box) => {
+          box.classList.remove("selected");
+        });
+  
+        this.classList.add("selected");
+        console.log(selectedValues);
       });
-
-      // Add 'active' class to the selected item
-      this.classList.add("active");
-      // Log the current state of the selectedValues object
-
-      console.log(selectedValues);
     });
-  });
-
-  // Additional code can be added here if you need to do something with the values, like sending them to a server
 });
+
+function moveBus() {
+    const busIcon = document.querySelector('.bus-icon');
+    busIcon.classList.add('move-right');
+
+    setTimeout(function() {
+        const queryParams = new URLSearchParams(selectedValues).toString();
+        window.location.href = `result.html?${queryParams}`;
+    }, 2000);
+}
